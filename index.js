a05adc766c09383802bef07d1e885b9cb830f341
erictherobot <erictherobot@gmail.com>
2016-10-11 17:17:48 -0400

    Adding Icon + Updating productName

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ function onClosed() {
 function createMainWindow() {
 	const win = new electron.BrowserWindow({
 		width: 600,
-		height: 400
+		height: 400,
+		icon: __dirname + '/electron.icns'
 	});
 
 	win.loadURL(`file://${__dirname}/index.html`);
