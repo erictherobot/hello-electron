7e2e9cf8da14326bacb7859f5fb72bc344490955
erictherobot <erictherobot@gmail.com>
2016-10-12 14:58:51 -0400

    Adding electron-reload package

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
+// Reload Electron if changes are detected upon file save. (Live Reload)
+require('electron-reload')(__dirname);
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
