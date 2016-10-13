b7479e8725c4d6ef3ea8372ca39f6082dcdde93a
erictherobot <erictherobot@gmail.com>
2016-10-13 09:28:01 -0400

    Adding Intro React Component

---
+++ src/assets/js/intro.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+var Intro = React.createClass({
+  toggleBgColor: function(){
+    document.getElementsByTagName('body')[0].classList.toggle('light');
+    document.getElementsByTagName('body')[0].classList.toggle('dark');
+  },
+  render: function() {
+    let classnames = 'btn btn-6 btn-6f';
+    return (
+      <header className="box">
+        <h1>Hello Electron</h1>
+        <p>I am a young desktop application ready to grow</p>
+        <button className={classnames} onClick={this.toggleBgColor}>I am a button</button>
+      </header>
+    );
+  }
+});
+
+ReactDOM.render(
+  <Intro />,
+  document.getElementById('intro')
+);
