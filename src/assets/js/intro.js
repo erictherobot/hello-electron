46f4208122425ba4818d711c556b829827473a22
erictherobot <erictherobot@gmail.com>
2016-10-23 13:17:24 -0400

    Adding Tone.js and binding the button to two functions for sound and color

--- src/assets/js/intro.js
+++ src/assets/js/intro.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Tone, { /*Sampler*/ } from "tone";
 
 var Intro = React.createClass({
+  playToneJS: function() {
+    var synth = new Tone.MembraneSynth().toMaster();
+    synth.triggerAttackRelease("C2", "8n");
+  },
   toggleBgColor: function() {
     document.getElementsByTagName('body')[0].classList.toggle('light');
     document.getElementsByTagName('body')[0].classList.toggle('dark');
+  },
+  onClick: function(event){
+      this.playToneJS();
+      this.toggleBgColor();
   },
   render: function() {
     const classnames = 'btn btn-6 btn-6f';
@@ -12,7 +21,7 @@ var Intro = React.createClass({
       <header className="box">
         <h1>Hello Electron</h1>
         <p>I am a young desktop application ready to grow</p>
-        <button className={classnames} onClick={this.toggleBgColor}>I am a big button</button>
+        <button className={classnames} onClick={this.onClick}>I am a sound button</button>
       </header>
     );
   }
